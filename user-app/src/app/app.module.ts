aa1833368ae06ccb922fe63d3c3d80b581cd5393
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-16 21:48:26 +0530

    day-2 [(ngModel)] component communication

--- user-app/src/app/app.module.ts
+++ user-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -11,7 +12,7 @@ import { UserInfoComponent } from './users/user-info/user-info.component';
     AppComponent, UsersComponent, UserImageComponent, UserInfoComponent
   ],
   imports: [                      // Module - Build-in or Custom module
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],                  // Service - Register the service with providers
   bootstrap: [AppComponent]       // Root Componant
