c5ce0f311ee7e6a62779424866c5dfc4b390b82c
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-18 18:44:47 +0530

    dynamic highlight directives

--- user-app/src/app/directives/highlight.directive.ts
+++ user-app/src/app/directives/highlight.directive.ts
@@ -1,12 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @Input() favColor : string;
+
+  @HostBinding('style.backgroundColor') bgColor : any;
+
+  @HostListener('mouseenter') onmouseenter(){
+    this.bgColor = this.favColor;
+  }
+
+  @HostListener('mouseleave') onmouseleave(){
+    this.bgColor = 'transparent';
+  }
+
   constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.backgroundColor = 'aqua';
+    // this.elementRef.nativeElement.style.backgroundColor = 'aqua';
    }
 
 }
