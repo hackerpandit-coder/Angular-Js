160dafb0b2f3f2e568f7fe155584c43b928e345c
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-18 18:23:27 +0530

    basic highlight & attribute directives added

--- user-app/src/app/app.module.ts
+++ user-app/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserImageComponent } from './users/user-image/user-image.component';
 import { UserInfoComponent } from './users/user-info/user-info.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [                 // Componant | Directives | Pipes
-    AppComponent, UsersComponent, UserImageComponent, UserInfoComponent
+    AppComponent, UsersComponent, UserImageComponent, UserInfoComponent, HighlightDirective
   ],
   imports: [                      // Module - Build-in or Custom module
     BrowserModule, FormsModule
--- user-app/src/app/users/user-info/user-info.component.ts
+++ user-app/src/app/users/user-info/user-info.component.ts
@@ -9,6 +9,15 @@ import { User } from 'src/app/model/user';
 export class UserInfoComponent implements OnInit {
 
   @Input() user : User;
+
+  myDynamicClass = {'my-border' : true, 'features' : false}
+  myDynamicStyle = { border : '2px red solid' }
+
+  onToggle(){
+    this.myDynamicClass['my-border']=!this.myDynamicClass['my-border'];
+    this.myDynamicClass['features']=!this.myDynamicClass['features'];
+  }
+
   constructor() { }
 
   ngOnInit(): void {
