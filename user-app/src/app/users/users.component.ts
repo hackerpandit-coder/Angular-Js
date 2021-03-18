ab3a6573035fd0af96c7c928d57be52abb5320fb
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-18 16:20:22 +0530

    life cycle methods

--- user-app/src/app/users/users.component.ts
+++ user-app/src/app/users/users.component.ts
@@ -1,23 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy,SimpleChanges } from '@angular/core';
 import { User } from '../model/user';
+import { USER_DATA } from '../data/mocks';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
 
-  user : User = {
-    firstName : "Bill",
-    lastName : "Gates",
-    dob : new Date("Dec 21, 1964"),
-    income : 50000,
-    isWorking : true,
-    company : "Microsoft",
-    image : "./assets/images/bill.jpg",
-    votes : 120
-  }
+  @Input() title: string;
+
+  user : User;
 
   onMoreInfo(user : User){
     alert(`Mr. ${user.lastName} is working with ${user.company}!!`);
@@ -26,6 +20,17 @@ export class UsersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.user = USER_DATA;
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges", changes);
+  }
+  ngDoCheck() { console.log("ngDoCheck") }
+  ngAfterContentInit() { console.log("ngAfterContentInit") }
+  ngAfterContentChecked() { console.log("ngAfterContentChecked") }
+  ngAfterViewInit() { console.log("ngAfterViewInit") }
+  ngAfterViewChecked() { console.log("ngAfterViewChecked") }
+  ngOnDestroy() { console.log("ngOnDestroy") }
 
 }
