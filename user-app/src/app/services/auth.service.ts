09430341b3127878bc22b7d17e9706e285267461
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-22 21:37:24 +0530

    firebase JWT token recieved

--- user-app/src/app/services/auth.service.ts
+++ user-app/src/app/services/auth.service.ts
@@ -15,4 +15,16 @@ export class AuthService {
     .then((response) => console.log(response))
     .catch(err => console.log("[FIREBASE RESPONSE]", err))
   }
+
+  loginUser(email : string, passwword : string){
+firebase.default
+.auth()
+.signInWithEmailAndPassword(email,passwword)
+.then((response) => {
+  firebase.default.auth().currentUser.getIdToken()
+  .then(token => console.log(token))
+  .catch(err => console.log(err))
+})
+.catch()
+  }
 }
