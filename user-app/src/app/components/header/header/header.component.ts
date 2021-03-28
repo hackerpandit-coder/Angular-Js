3d72fda331a2c7fc4b01fb89d30a59fe6a8b794b
hackerpandit-coder <virendramishra1995@gmail.com>
2021-03-28 15:58:54 +0530

    logout feature added

--- user-app/src/app/components/header/header/header.component.ts
+++ user-app/src/app/components/header/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ export class HeaderComponent implements OnInit {
 
   tab : number = 1;
 
-  constructor() { }
+  constructor(public authService : AuthService) { }
 
   ngOnInit(): void {
   }
